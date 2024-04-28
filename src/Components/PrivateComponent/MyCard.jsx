e6c42ba95ece30b852e81d9f590580e71613cc6b
Shakil4432 <shakil4432@gmail.com>
2024-04-28 21:39:18 +0600

    added sweetAlert

--- src/Components/PrivateComponent/MyCard.jsx
+++ src/Components/PrivateComponent/MyCard.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { MdDelete } from "react-icons/md";
+import { TbHttpDelete } from "react-icons/tb";
 
-export default function MyCard({ itemCard }) {
+export default function MyCard({ itemCard, handleDelete }) {
     const { _id, itemName, price, rating, imgURL, stock } = itemCard
+
+    
+
     return (
         <div>
-            <div className="card card-compact w-96 bg-base-100 shadow-xl">
+            <div className="card card-compact w-96 bg-base-100 shadow-xl relative">
                 <figure><img src={imgURL} className='w-full h-[35vh]' /></figure>
-                <div className="card-body">
+                <div className="card-body ">
                     <h2 className="card-title">{itemName}</h2>
                     <p><span className='font-bold'>PRICE </span>: {price}TK</p>
                     <p><span className='font-bold'>RATING </span>: {rating}</p>
@@ -21,10 +26,10 @@ export default function MyCard({ itemCard }) {
                         </div>
                         <div className='flex items-center justify-center gap-4'>
                             <div className="card-actions justify-end">
-                                <Link  className="btn btn-accent btn-circle text-white">Update</Link>
+                                <Link  className="btn btn-accent  text-white">Update</Link>
                             </div>
-                            <div className="card-actions justify-end">
-                                <Link  className="btn btn-error btn-circle text-white">Delete</Link>
+                            <div className="card-actions absolute top-[-5%] right-[-5%] text-red-400 ">
+                                <Link onClick={()=>handleDelete(_id)}  className="btn btn-error btn-circle text-3xl"><TbHttpDelete></TbHttpDelete></Link>
                             </div>
                         </div>
                     </div>
--- src/Components/PrivateComponent/MyItems.jsx
+++ src/Components/PrivateComponent/MyItems.jsx
@@ -1,20 +1,53 @@
-import React, { useContext} from 'react'
+import React, { useContext, useState } from 'react'
 import { useLoaderData } from 'react-router-dom'
 import { AuthContext } from '../AuthProvider/AuthProvider';
 import MyCard from './MyCard';
+import Swal from 'sweetalert2';
 
 export default function MyItems() {
- 
-  const {user} = useContext(AuthContext);
+
+  const { user } = useContext(AuthContext);
   const allCards = useLoaderData();
   const allItems = allCards.filter(card => card.email === user.email);
-  
+  const [items, setItems] = useState(allItems);
+
+  const handleDelete = id => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/JuteAndWoodenCraft/${id}`, {
+          method: "DELETE",
+        })
+          .then(res => res.json())
+          .then(data => {
+            if (data.deletedCount > 0) {
+              const remainingItems = items.filter(item => item._id !== id)
+              setItems(remainingItems)
+            }
+          })
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success"
+        });
+      }
+    });
+
+  }
+
   return (
     <div className='mt-16'>
       <h2 className='text-4xl font-bold mb-10 text-center'>My Jute And Wooden Craft Items</h2>
       <div className='flex justify-center items-center gap-6 flex-wrap'>
         {
-          allItems.map(itemCard=> <MyCard key={itemCard._id} itemCard={itemCard}></MyCard>)
+          items.map(itemCard => <MyCard key={itemCard._id} handleDelete={handleDelete} itemCard={itemCard}></MyCard>)
         }
       </div>
     </div>
